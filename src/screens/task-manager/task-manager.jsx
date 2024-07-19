045c82a44f67a2bc58dbfce1f48fa2f7dbd611ec
shoaib8236 <shoaib.raza.dev@gmail.com>
2024-07-19 17:17:07 +0500

    task manager fix

--- src/screens/task-manager/task-manager.jsx
+++ src/screens/task-manager/task-manager.jsx
@@ -79,90 +79,91 @@ const TaskManager = () => {
 
   return (
     <div className="h-full py-screen flex flex-col overflow-hidden">
-      <div className="content-wrap px-0 mb-6">
-        <h1>Task Manager</h1>
-        <div className="flex w-max ml-auto gap-4 relative z-50">
-          <button
-            onClick={onCopyText}
-            className="flex bg-secondary font-semibold px-6 py-2 items-center rounded-2xl gap-2 ml-auto"
-          >
-            <BiCopy className="text-2xl" />
-          </button>
-          <button
-            onClick={onDeleteSaved}
-            className="flex bg-secondary font-semibold px-6 py-2 items-center rounded-2xl gap-2 ml-auto"
-          >
-            <BiTrash className="text-2xl" />
-          </button>
-          <button
-            onClick={onSave}
-            className="flex bg-secondary font-semibold px-6 py-2 items-center rounded-2xl gap-2 ml-auto"
-          >
-            <AiFillSave className="text-2xl" />
-          </button>
-          <button
-            onClick={toggleTaskAdd}
-            className="flex bg-secondary font-semibold px-6 py-2 items-center rounded-2xl gap-2 ml-auto"
-          >
-            {tasks?.length > 0 ? (
-              <span>
-                <BiEdit className="text-2xl" />
-              </span>
-            ) : (
-              <BiPlus
-                className={`duration-200 ${isTaskAdd ? "rotate-45" : ""}`}
-              />
-            )}
-          </button>
+      <div className="content-wrap">
+        <div className="content-wrap px-0 mb-6">
+          <h1>Task Manager</h1>
+          <div className="flex w-max ml-auto gap-4 relative z-50">
+            <button
+              onClick={onCopyText}
+              className="flex bg-secondary font-semibold px-6 py-2 items-center rounded-2xl gap-2 ml-auto"
+            >
+              <BiCopy className="text-2xl" />
+            </button>
+            <button
+              onClick={onDeleteSaved}
+              className="flex bg-secondary font-semibold px-6 py-2 items-center rounded-2xl gap-2 ml-auto"
+            >
+              <BiTrash className="text-2xl" />
+            </button>
+            <button
+              onClick={onSave}
+              className="flex bg-secondary font-semibold px-6 py-2 items-center rounded-2xl gap-2 ml-auto"
+            >
+              <AiFillSave className="text-2xl" />
+            </button>
+            <button
+              onClick={toggleTaskAdd}
+              className="flex bg-secondary font-semibold px-6 py-2 items-center rounded-2xl gap-2 ml-auto"
+            >
+              {tasks?.length > 0 ? (
+                <span>
+                  <BiEdit className="text-2xl" />
+                </span>
+              ) : (
+                <BiPlus
+                  className={`duration-200 ${isTaskAdd ? "rotate-45" : ""}`}
+                />
+              )}
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="content-wrap bg-gray-50 dark:bg-dark-background-secondary hide-scrollbar py-6 rounded-xl flex-grow overflow-auto">
-        <div>
-          {isTaskAdd ? (
-            <div className="">
-              <div className="flex flex-col items-center gap-4">
-                <textarea
-                  value={textareaValue}
-                  onChange={onTextareaChange}
-                  className="hide-scrollbar bg-gray-200 dark:bg-dark-background-secondary p-4 w-full max-w-[600px] min-h-[300px] rounded-2xl"
-                ></textarea>
-                <button
-                  onClick={onSaveTextarea}
-                  className="flex bg-secondary font-semibold px-6 py-2 items-center rounded-2xl gap-2"
-                >
-                  <BiSave className="text-2xl" /> Save Tasks
-                </button>
-              </div>
-            </div>
-          ) : null}
-          {!isTaskAdd && (
-            <div>
-              {tasks.map((task, index) => (
-                <div
-                  className="flex justify-between border-t first:border-none py-4 border-primary"
-                  key={task?.id + index + 1}
-                >
-                  <span className="font-semibold">
-                    {task?.done ? <del>{task?.task}</del> : task?.task}
-                  </span>
-                  <div className="flex items-center gap-4">
-                    <span className="text-xs text-gray-500 dark:text-dark-text-secondary">
-                      {moment(task?.updatedAt).format(constants.DATE_FORMAT)}
-                    </span>
-                    <span>
-                      <input
-                        value={task?.done}
-                        checked={task?.done}
-                        onChange={() => onDone(task?.id)}
-                        type="checkbox"
-                      />
-                    </span>
-                  </div>
+        <div className="content-wrap bg-gray-50 dark:bg-dark-background-secondary hide-scrollbar py-6 rounded-xl flex-grow overflow-auto">
+          <div>
+            {isTaskAdd ? (
+              <div className="">
+                <div className="flex flex-col items-center gap-4">
+                  <textarea
+                    value={textareaValue}
+                    onChange={onTextareaChange}
+                    className="hide-scrollbar bg-gray-200 dark:bg-dark-background-secondary p-4 w-full max-w-[600px] min-h-[300px] rounded-2xl"
+                  ></textarea>
+                  <button
+                    onClick={onSaveTextarea}
+                    className="flex bg-secondary font-semibold px-6 py-2 items-center rounded-2xl gap-2"
+                  >
+                    <BiSave className="text-2xl" /> Save Tasks
+                  </button>
                 </div>
-              ))}
-            </div>
-          )}
+              </div>
+            ) : null}
+            {!isTaskAdd && (
+              <div>
+                {tasks.map((task, index) => (
+                  <div
+                    className="flex justify-between border-t first:border-none py-4 border-primary"
+                    key={task?.id + index + 1}
+                  >
+                    <span className="font-semibold">
+                      {task?.done ? <del>{task?.task}</del> : task?.task}
+                    </span>
+                    <div className="flex items-center gap-4">
+                      <span className="text-xs text-gray-500 dark:text-dark-text-secondary">
+                        {moment(task?.updatedAt).format(constants.DATE_FORMAT)}
+                      </span>
+                      <span>
+                        <input
+                          value={task?.done}
+                          checked={task?.done}
+                          onChange={() => onDone(task?.id)}
+                          type="checkbox"
+                        />
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
